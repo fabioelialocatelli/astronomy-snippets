155c3a24502f65cc7b31e98c81954c5eb2a76998
Fabio Elia Locatelli <locatelli.fabio.elia@gmail.com>
2016-08-09 21:56:30 +1200

    First object oriented release

--- SimbadConnector/SimbadConnector/src/simbadConnector/SimbadConnector.java
+++ SimbadConnector/SimbadConnector/src/simbadConnector/SimbadConnector.java
@@ -15,21 +15,44 @@ import org.jsoup.nodes.Document;
 
 public class SimbadConnector {
 
-	public static void main(String[] args) {
+	private int connectionCode = 0;
+	private URL descriptionURL = null;
+	private HttpURLConnection sourceConnection = null;
+	private BufferedReader source = null;
+	private StringBuilder unrefinedHTML = null;
+	private String precedingData = null;
 
-		int connectionCode = 0;
-		URL descriptionURL = null;
-		HttpURLConnection sourceConnection = null;
-		BufferedReader source = null;
-		StringBuilder unrefinedHTML = null;
-		String option = null;
-		String precedingData = null;
+	private Document page = null;
+	private Connection bayerConnection = null;
+	private Connection flamsteedConnection = null;
+	private PreparedStatement databaseManipulation = null;
 
-		Document page = null;
-		Connection bayerConnection = null;
-		Connection flamsteedConnection = null;
-		PreparedStatement databaseManipulation = null;
+	private String[] bayerLetters = { "alf", "bet", "gam", "del", "eps", "zet", "eta", "tet", "iot", "kap", "lam", "mu",
+			"nu", "ksi", "omi", "pi", "rho", "sig", "tau", "ups", "phi", "chi", "psi", "ome" };
 
+	private String[] bayerNumbers = { "0", "1", "2", "3", "4", "5" };
+
+	private String[] bayerConstellations = { "and", "ant", "aps", "aqr", "aql", "ara", "ari", "aur", "boo", "cae",
+			"cnc", "cma", "cap", "car", "cas", "cen", "cep", "cet", "cha", "cir", "col", "cra", "crb", "crv", "crt",
+			"cru", "cyg", "dor", "dra", "eri", "for", "gem", "gru", "her", "hor", "hya", "hyi", "ind", "leo", "lep",
+			"lib", "lup", "lyr", "men", "mic", "mus", "nor", "oct", "oph", "ori", "pav", "peg", "per", "phe", "pic",
+			"psc", "psa", "pup", "pyx", "ret", "sgr", "sco", "scl", "sct", "ser", "tel", "tra", "tuc", "uma", "umi",
+			"vel", "vir", "vol" };
+
+	private String[] flamsteedNumbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14",
+			"15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32",
+			"33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+			"51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66", "67", "68",
+			"69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83", "84", "85", "86",
+			"87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100", "101", "102", "103",
+			"104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115", "116", "117", "118",
+			"119", "120", "121", "122", "123", "124", "125", "126", "127", "128", "129", "130", "131", "132", "133",
+			"134", "135", "136", "137", "138", "139", "140", "141", "142", "143", "144", "145" };
+
+	private String[] flamsteedConstellations = { "cam", "cvn", "cmi", "com", "del", "equ", "lac", "lmi", "lyn", "mon",
+			"sge", "sex", "tau", "tri", "vul" };
+
+	public void anchorDatabases() {
 		try {
 			bayerConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bayer?characterEncoding=utf8",
 					"root", "HadarToliman@#$");
@@ -38,35 +61,9 @@ public class SimbadConnector {
 		} catch (SQLException connectionError) {
 			connectionError.getMessage();
 		}
+	}
 
-		String[] bayerLetters = { "alf", "bet", "gam", "del", "eps", "zet", "eta", "tet", "iot", "kap", "lam", "mu",
-				"nu", "ksi", "omi", "pi", "rho", "sig", "tau", "ups", "phi", "chi", "psi", "ome" };
-
-		String[] bayerNumbers = { "0", "1", "2", "3", "4", "5" };
-
-		String[] bayerConstellations = { "and", "ant", "aps", "aqr", "aql", "ara", "ari", "aur", "boo", "cae", "cnc",
-				"cma", "cap", "car", "cas", "cen", "cep", "cet", "cha", "cir", "col", "cra", "crb", "crv", "crt", "cru",
-				"cyg", "dor", "dra", "eri", "for", "gem", "gru", "her", "hor", "hya", "hyi", "ind", "leo", "lep", "lib",
-				"lup", "lyr", "men", "mic", "mus", "nor", "oct", "oph", "ori", "pav", "peg", "per", "phe", "pic", "psc",
-				"psa", "pup", "pyx", "ret", "sgr", "sco", "scl", "sct", "ser", "tel", "tra", "tuc", "uma", "umi", "vel",
-				"vir", "vol" };
-
-		String[] flamsteedNumbers = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15",
-				"16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32",
-				"33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
-				"50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "60", "61", "62", "63", "64", "65", "66",
-				"67", "68", "69", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "80", "81", "82", "83",
-				"84", "85", "86", "87", "88", "89", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "100",
-				"101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "111", "112", "113", "114", "115",
-				"116", "117", "118", "119", "120", "121", "122", "123", "124", "125", "126", "127", "128", "129", "130",
-				"131", "132", "133", "134", "135", "136", "137", "138", "139", "140", "141", "142", "143", "144",
-				"145" };
-
-		String[] flamsteedConstellations = { "cam", "cvn", "cmi", "com", "del", "equ", "lac", "lmi", "lyn", "mon",
-				"sge", "sex", "tau", "tri", "vul" };
-
-		option = "bayer";
-
+	public void editDatabase(String option) {
 		switch (option) {
 
 		case "bayer":
@@ -911,5 +908,12 @@ public class SimbadConnector {
 			}
 			break;
 		}
+	}
+
+	public static void main(String[] args) {
+
+		SimbadConnector simbadConnector = new SimbadConnector();
+		simbadConnector.anchorDatabases();
+		simbadConnector.editDatabase("");
 	}
 }
